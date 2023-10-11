7e848e3f47c03d2bb7f1f983f02f3e443ee2a021
Adam Rosas <adamrosas27@gmail.com>
2023-10-11 15:19:17 -0700

    Add comments displaying the tentative Graph class structure

---
+++ src/graph.py
@@ -0,0 +1,19 @@
+# class Graph:
+
+# def __init__(self, verticies):
+# pass
+
+# def add_vertex(self, vertex):
+# pass
+
+# def add_edge(self, vertex1, vertex2):
+# pass
+
+# get_edge_weight(self, vertex1, vertex2):
+# pass
+
+# prims():
+# pass
+
+# get_all_edges_and_total_weight(self):
+# pass
