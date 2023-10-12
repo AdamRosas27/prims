306efdc232b696bf174af7a9546a97207ea88c7a
Adam Rosas <adamrosas27@gmail.com>
2023-10-11 18:14:12 -0700

    Add better test cases and more print functionalities

--- src/main.py
+++ src/main.py
@@ -6,15 +6,48 @@ class Main:
 
     def main(self):
         g = Graph()
-        g.add_vertex(0)
         g.add_vertex(1)
         g.add_vertex(2)
         g.add_vertex(3)
-        g.add_edge(0, 1, 10)
-        g.add_edge(1, 2, 15)
-        g.add_edge(2, 3, 20)
+        g.add_vertex(4)
+        g.add_vertex(5)
+        g.add_vertex(6)
+        g.add_vertex(7)
+        g.add_edge(1, 2, 2)
+        g.add_edge(1, 3, 4)
+        g.add_edge(1, 6, 5)
+        g.add_edge(1, 4, 7)
+        g.add_edge(2, 1, 2)
+        g.add_edge(2, 4, 6)
+        g.add_edge(2, 7, 8)
+        g.add_edge(2, 5, 3)
+        g.add_edge(3, 1, 4)
+        g.add_edge(3, 6, 6)
+        g.add_edge(4, 1, 7)
+        g.add_edge(4, 2, 6)
+        g.add_edge(4, 6, 1)
+        g.add_edge(4, 7, 6)
+        g.add_edge(5, 2, 3)
+        g.add_edge(5, 7, 7)
+        g.add_edge(6, 3, 6)
+        g.add_edge(6, 1, 5)
+        g.add_edge(6, 4, 1)
+        g.add_edge(6, 7, 6)
+        g.add_edge(7, 6, 6)
+        g.add_edge(7, 4, 6)
+        g.add_edge(7, 2, 8)
+        g.add_edge(7, 5, 7)
         g.get_edge_weight(0, 1)
         g.get_all_edges_and_weights()
+        mst = g.prims(1)
+        print("\nMinimum Spanning Tree (MST):")
+        for u, v, weight in mst:
+            print(
+                f"Edge: {u}-{v}, Weight: {weight}")
+        total_mst_weight = 0
+        for _, _, weight in mst:
+            total_mst_weight += weight
+        print(f"\nTotal MST Weight: {total_mst_weight}")
 
 
 # if __name__ == '__main__':
